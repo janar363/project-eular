618cede445f7d9940822cc60d452016acc18288d
janar363 <59265023+janar363@users.noreply.github.com>
2024-01-12 13:00:33 -0600

    added sol 10

---
+++ 10.cpp
@@ -0,0 +1,51 @@
+# include <iostream>
+# include <chrono>
+
+using namespace std;
+using namespace chrono;
+
+vector<long long> primes;
+
+bool is_prime(int n){
+  for(int i=2; i<=sqrt(n); i++){
+    if(n%i==0) return false;
+  }
+  return true;
+}
+
+void sol(){
+  long n; cin >> n;
+
+  if(primes[primes.size()-1] < n){
+    for(long i=primes[primes.size()-1] == 2? primes[primes.size()-1]+1:primes[primes.size()-1]+2; primes.size() < n; i+=2){
+      if(is_prime(i)) primes.push_back(i);
+    }
+    
+  }
+
+  long long psum = 0ll;
+  for(int i=0; primes[i] <= n; i++)
+    psum += primes[i];
+
+  cout << psum << endl;
+}
+
+int main() {
+
+  high_resolution_clock::time_point t1 = high_resolution_clock::now();
+  primes.push_back(2);
+  int t = 1;
+  
+  cin >> t;
+
+  while(t--)
+    sol();
+  
+  high_resolution_clock::time_point t2 = high_resolution_clock::now();
+
+  auto duration = duration_cast<microseconds>( t2 - t1 ).count();
+
+  cout << "Program run time: " << duration / 1000000.0 << " seconds" << endl;
+
+  return 0;
+}

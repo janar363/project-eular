6886eb0bf11d28a8e13df528d73824efb572a484
janar363 <59265023+janar363@users.noreply.github.com>
2024-01-17 16:27:10 -0600

    added sol 21

---
+++ 21.cpp
@@ -0,0 +1,44 @@
+# include <iostream>
+# include <unordered_map>
+# include <math.h>
+
+using namespace std;
+
+unordered_map<int, long long> dp;
+
+void sol(){
+  int n; cin >> n;
+
+  long long sum = 0;
+  for(int i=1; i<=n; i++){
+    if(i != dp[i] && dp[dp[i]] == i) sum += i;
+  }
+
+  cout << sum << endl;
+  // cout << dp[220] << endl;
+}
+
+int main() {
+  
+  for(int i=1; i<=100000; i++){
+    long long divisorSum = 0;
+    for(int div=1; div<=sqrt(i); div++){
+      if(i % div == 0) {
+        divisorSum += div;
+        int quotient = i / div;
+
+        if(div != quotient and quotient != i) divisorSum += quotient; 
+      }
+    }
+    dp[i] = divisorSum;
+  }
+
+  int t = 1;
+  
+  cin >> t;
+
+  while(t--)
+  sol();
+
+  return 0;
+}
